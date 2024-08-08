eb64317567a0dc0b02dafcedf68bf5092d4876f9
Frederico Soares Figueiredo <fred.webfront@gmail.com>
2024-08-08 16:18:51 -0300

    refactor: moves WINNING_CONDITIONS constant into a file

---
+++ constants.ts
@@ -0,0 +1,34 @@
+export const WINNING_CONDITIONS = [
+  {
+    name: "horizontal-1",
+    tiles: [0, 1, 2],
+  },
+  {
+    name: "horizontal-2",
+    tiles: [3, 4, 5],
+  },
+  {
+    name: "horizontal-3",
+    tiles: [6, 7, 8],
+  },
+  {
+    name: "vertical-1",
+    tiles: [0, 3, 6],
+  },
+  {
+    name: "vertical-2",
+    tiles: [1, 4, 7],
+  },
+  {
+    name: "vertical-3",
+    tiles: [2, 5, 8],
+  },
+  {
+    name: "diagonal-1",
+    tiles: [0, 4, 8],
+  },
+  {
+    name: "diagonal-2",
+    tiles: [2, 4, 6],
+  },
+]
